48b6017e1c3e6ce1eae314e2171073f4c2e81eab
Darshitpipariya <darshit2272000@gmail.com>
2023-03-13 22:38:36 +0530

    Updated Pom.xml with log dependency and build configuration added log4j2.xml and added Square root functionality

--- src/main/java/org/Scientific_Calculator/App.java
+++ src/main/java/org/Scientific_Calculator/App.java
@@ -1,20 +1,49 @@
 package org.Scientific_Calculator;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
-/**
- * Hello world!
- *
- */
 public class App 
 {
+    private static final Logger logger =LogManager.getLogger(App.class);
+    public App(){
+
+    }
     public static void main( String[] args )
     {
+        App app=new App();
         Scanner sc=new Scanner(System.in);
-        int n=sc.nextInt();
-
-        App obj=new App();
-        System.out.println(obj.factorial(n));
+        double num1,num2;
+        do{
+            System.out.println("Scientific Calculator ,Choose to perform Operation");
+            System.out.println("1: Square root function\n");
+            int choice;
+            try{
+                choice= sc.nextInt();
+            }catch (InputMismatchException e){
+                return ;
+            }
+            switch (choice){
+                case 1:
+                    // Square root function
+                    System.out.println("Enter the number : ");
+                    num1= sc.nextDouble();
+                    System.out.println("Square root of "+num1+" is "+app.squareRoot(num1));
+                    System.out.println("");
+                    break;
+                default:
+                    System.out.println("Exit");
+                    return;
+            }
+        }while (true);
+    }
+    public double squareRoot(double num1){
+        logger.info("[SQUARE ROOT] - "+num1);
+        double result=Math.sqrt(num1);
+        logger.info("[SQUARE ROOT] - "+num1);
+        return result;
     }
     public int factorial(int n){
         if(n==1){
--- src/test/java/org/Scientific_Calculator/AppTest.java
+++ src/test/java/org/Scientific_Calculator/AppTest.java
@@ -3,18 +3,34 @@ package org.Scientific_Calculator;
 
 import org.junit.Test;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 
 /**
  * Unit test for simple App.
  */
 public class AppTest
 {
+    private static final double DELTA= 1e-15;
     App obj=new App();
+    @Test
+    public void squareTruePositive(){
+        assertEquals("Finding Square root of a number for True Positive",4,obj.squareRoot(16),DELTA);
+        assertEquals("Finding Square root of a number for True Positive",2,obj.squareRoot(4),DELTA);
+    }
+    @Test
+    public void squareFalsePositive(){
+        assertNotEquals("Finding Square root of a number for false Positive",2,obj.squareRoot(16),DELTA);
+        assertNotEquals("Finding Square root of a number for false Positive",1,obj.squareRoot(4),DELTA);
+    }
     @Test
     public void factorialTruePositive(){
         assertEquals("Finding factorial of a number for True Positive",120,obj.factorial(5));
-
-        assertEquals("Finding factorial of a number for True Positive",24,obj.factorial(4));    }
+        assertEquals("Finding factorial of a number for True Positive",24,obj.factorial(4));
+    }
+    @Test
+    public void factorialFalsePositive(){
+        assertNotEquals("Finding factorial of a number for false Positive",100,obj.factorial(5));
+        assertNotEquals("Finding factorial of a number for false Positive",20,obj.factorial(4));
+    }
 
 }
