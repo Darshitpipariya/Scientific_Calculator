416c8b87694f2e5be522b47c25c0a22044ab3f28
Darshitpipariya <darshit2272000@gmail.com>
2023-03-14 23:50:23 +0530

    added power function and test cases for al functionalities

--- src/main/java/org/Scientific_Calculator/App.java
+++ src/main/java/org/Scientific_Calculator/App.java
@@ -18,7 +18,11 @@ public class App
 
         do{
             System.out.println("Scientific Calculator ,Choose to perform Operation");
-            System.out.println("1: Square root\n2: Factorial\n3: Natural logarithm");
+            System.out.println("1: Square root\n" +
+                    "2: Factorial\n" +
+                    "3: Natural logarithm\n" +
+                    "4: Power function\n" +
+                    "other: exit\n");
             int choice;
             int n;
             double num1,num2;
@@ -49,6 +53,15 @@ public class App
                     System.out.println("Natural log of "+num1+" is "+app.naturalLog(num1));
                     System.out.println("");
                     break;
+                case 4:
+                    //Natural Power function
+                    System.out.println("Enter the num : ");
+                    num1=sc.nextDouble();
+                    System.out.println("\nEnter the power : ");
+                    num2=sc.nextDouble();
+                    System.out.println(num1+" power of "+num2+" is "+app.power(num1,num2));
+                    System.out.println("");
+                    break;
                 default:
                     System.out.println("Exit");
                     return;
@@ -71,6 +84,12 @@ public class App
         logger.info("[NATURAL LOGARITHM OF] - "+n);
         double result=Math.log(n);
         logger.info("[NATURAL LOGARITHM RESULT] - "+result);
+        return result;
+    }
+    public double power(double n,double p){
+        logger.info("[ "+p+" POWER OF "+n+"]");
+        double result=Math.pow(n,p);
+        logger.info("[ "+p+" POWER OF "+n+" RESULT] - "+result);
         return result;
     }
     public int factorialCal(int n){
--- src/test/java/org/Scientific_Calculator/AppTest.java
+++ src/test/java/org/Scientific_Calculator/AppTest.java
@@ -32,5 +32,25 @@ public class AppTest
         assertNotEquals("Finding factorial of a number for false Positive",100,obj.factorial(5));
         assertNotEquals("Finding factorial of a number for false Positive",20,obj.factorial(4));
     }
+    @Test
+    public void logTruePositive(){
+        assertEquals("Finding Natural log of a number for True Positive",2.302585092994046,obj.naturalLog(10),DELTA);
+        assertEquals("Finding Natural log of a number for True Positive",1.3862943611198906,obj.naturalLog(4),DELTA);
+    }
+    @Test
+    public void logFalsePositive(){
+        assertNotEquals("Finding Natural log of a number for false Positive",2.3025,obj.naturalLog(16),DELTA);
+        assertNotEquals("Finding Natural log of a number for false Positive",1.2862,obj.naturalLog(20),DELTA);
+    }
+    @Test
+    public void powerTruePositive(){
+        assertEquals("Finding POWER of a number for True Positive",9765625,obj.power(5,10),DELTA);
+        assertEquals("Finding POWER of a number for True Positive",1024,obj.power(4,5),DELTA);
+    }
+    @Test
+    public void powerFalsePositive(){
+        assertNotEquals("Finding POWER of a number for false Positive",100,obj.power(5,10),DELTA);
+        assertNotEquals("Finding POWER of a number for false Positive",102,obj.power(4,5),DELTA);
+    }
 
 }
